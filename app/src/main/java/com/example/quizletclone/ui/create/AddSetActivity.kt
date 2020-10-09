9d3d0231200824a58ce7a9d13373ea34d6a9caf9
slscott9 <baseballdude9cf@gmail.com>
2020-10-09 17:38:02 -0500

    d

--- app/src/main/java/com/example/quizletclone/ui/create/AddSetActivity.kt
+++ app/src/main/java/com/example/quizletclone/ui/create/AddSetActivity.kt
@@ -2,13 +2,17 @@ package com.example.quizletclone.ui.create
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ScrollView
 import androidx.databinding.DataBindingUtil
 import com.example.quizletclone.R
 import com.example.quizletclone.databinding.ActivityAddSetBinding
+import kotlinx.android.synthetic.main.activity_add_set.*
 
 class AddSetActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddSetBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +22,9 @@ class AddSetActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolBar)
         supportActionBar?.setHomeButtonEnabled(true)
 
-
         binding.fabAddSetTerm.setOnClickListener {
            addTermDef()
+
         }
 
     }
@@ -29,5 +33,7 @@ class AddSetActivity : AppCompatActivity() {
         val parentLayout = binding.termDefContainer
         val childLayout = layoutInflater.inflate(R.layout.term_definition_item, parentLayout, false)
         parentLayout.addView(childLayout)
+        binding.scrollView.fullScroll(View.FOCUS_DOWN)
+
     }
 }
